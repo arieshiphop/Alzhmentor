e30c64224b2823bcc85ac06ce37b49541c87e684
AriesHH <ikerakaaries@gmail.com>
2022-03-25 00:37:03 +0100

    ..

---
+++ front/src/coins.js
@@ -0,0 +1,16 @@
+export default {
+    coins: [
+        { "value": 50, "img": "https://static2.abc.es/media/economia/2017/04/03/50_1a-kGgB--420x236@abc.jpg" },
+        { "value": 20, "img": "http://www.europaciudadana.org/wp-content/uploads/2015/10/FEC_Nuevo20euros_blog.jpg" },
+        { "value": 10, "img": "https://cdn.shortpixel.ai/spai/w_898+q_lossy+ret_img/https://www.eude.ec/wp-content/uploads/sites/7/2014/09/10-euros-820x450.jpg" },
+        { "value": 5, "img": "https://img.milanuncios.com/fg/2600/07/260007960_1.jpg?VersionId=_Nwomy4kzErqE0yugVOm1otP40NS9iau" },
+        { "value": 2, "img": "https://los10.org/wp-content/uploads/2021/02/DOSEUROS.jpg" },
+        { "value": 1, "img": "https://w7.pngwing.com/pngs/106/996/png-transparent-1-euro-coin-euro-coins-female-leg-miscellaneous-female-leg-1-cent-euro-coin.png" },
+        { "value": 0.50, "img": "https://img2.freepng.es/20180205/scw/kisspng-50-cent-euro-coin-euro-coins-euro-coin-png-image-5a7803ce65c250.4713586115178147344168.jpg" },
+        { "value": 0.20, "img": "https://w7.pngwing.com/pngs/927/558/png-transparent-20-euro-cent-coin-20-cent-euro-coin-euro-coins-euro-coin-label-material-cash-thumbnail.png" },
+        { "value": 0.10, "img": "https://e7.pngegg.com/pngimages/320/415/png-clipart-10-euro-cent-coin-10-euro-note-1-cent-euro-coin-20-cent-euro-coin-payment-gold-thumbnail.png" },
+        { "value": 0.05, "img": "https://img2.freepng.es/20180519/kie/kisspng-5-cent-euro-coin-1-cent-euro-coin-nickel-5b0037550a81b9.142716971526740821043.jpg" },
+        { "value": 0.02, "img": "https://w7.pngwing.com/pngs/877/533/png-transparent-2-cent-euro-coin-1-cent-euro-coin-euro-coins-1-euro-coin-euro-medal-material-cash.png" },
+        {"value":0.01,"img":"https://cronicaglobal.elespanol.com/uploads/s1/16/85/38/4/moneda-centimo.png"}
+    ]
+}
